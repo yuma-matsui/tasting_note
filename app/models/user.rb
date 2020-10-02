12062ae686103ef68ee8bb2158cd304eec539f5a
yuma-matsui <oshino.iiyo@gmail.com>
2020-10-02 14:15:20 +0900

    ログイン、サインアップ画面作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, format: { with: /\A[a-z\d]{4,}+\z/i }
   has_many :wines
 end
