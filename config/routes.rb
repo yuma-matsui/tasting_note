0116a0e2c115e2086256a91f7916287ed8157c6a
yuma-matsui <oshino.iiyo@gmail.com>
2020-10-02 19:05:02 +0900

    winesテーブルに必要な3テーブルのcreateアクションのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "wines#index"
-  resources :wines, only: [:index]
+  resources :wines, only: [:index, :new, :create] do
+    resources :looks, only: [:create]
+    resources :flavors, only: [:create]
+    resources :tastes, only: [:create]
+  end
 end
