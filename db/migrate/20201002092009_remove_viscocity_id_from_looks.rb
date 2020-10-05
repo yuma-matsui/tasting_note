a77eda43102ddd2ada2665a526f051d70752198e
yuma-matsui <oshino.iiyo@gmail.com>
2020-10-05 17:15:09 +0900

    微調整

--- db/migrate/20201002092009_remove_viscocity_id_from_looks.rb
+++
@@ -1,5 +0,0 @@
-class RemoveViscocityIdFromLooks < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :looks, :viscocity_id, :integer
-  end
-end
--- db/migrate/20201002092327_add_viscosity_id_to_looks.rb
+++
@@ -1,5 +0,0 @@
-class AddViscosityIdToLooks < ActiveRecord::Migration[6.0]
-  def change
-    add_column :looks, :viscosity_id, :integer, null: false
-  end
-end
