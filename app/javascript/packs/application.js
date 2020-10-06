0c418ad5066a1bad2933a12ff31f7920bd58d441
yuma-matsui <oshino.iiyo@gmail.com>
2020-10-06 12:11:06 +0900

    コンセプト部分作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("../menu_bar")
 require("../user_menu")
+require("../concept")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
