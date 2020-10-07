e09de70d9fb99b3c57b9268db416c80007fc468e
yuma-matsui <oshino.iiyo@gmail.com>
2020-10-07 12:54:46 +0900

    トップページ見出し部分のイメージスライド機能実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 require("../menu_bar")
 require("../user_menu")
 require("../concept")
+require("../slide_image")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
