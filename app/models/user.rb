bb600c79dae1860ae47a196eaefe68a19fda5723
yuma-matsui <oshino.iiyo@gmail.com>
2020-10-09 15:35:10 +0900

    微調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true, uniqueness: true, format: { with: /\A[a-z\d]{4,}+\z/i }
