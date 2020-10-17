c596a9d93819811d69a2551b8296b72af3847772
yuma-matsui <oshino.iiyo@gmail.com>
2020-10-17 09:59:54 +0900

    お気に入り削除機能追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,17 @@
 class FavoritesController < ApplicationController
-  before_action :wine_find, only: [:create]
+  before_action :wine_find, only: [:create, :destroy]
 
   def create
     favorite = Favorite.create(user_id: current_user.id, wine_id: @wine.id)
     redirect_to wine_path(@wine)
   end
+
+  def destroy
+    favorite = Favorite.find_by(user_id: current_user.id, wine_id: @wine.id)
+    favorite.destroy
+    redirect_to wine_path(@wine)
+  end
+
 
   private
 
