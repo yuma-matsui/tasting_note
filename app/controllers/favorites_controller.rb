e0e206273ef6e57a4cc0b299cd1a755f454516ad
yuma-matsui <oshino.iiyo@gmail.com>
2020-10-17 09:49:08 +0900

    お気に入り追加機能追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,9 @@
 class FavoritesController < ApplicationController
-  before_action :wine_find, only: [:create, :destroy]
+  before_action :wine_find, only: [:create]
 
   def create
-    @favorite = Favorite.new(user_id: current_user.id, wine_id: @wine.id)
-    @favorite.save
-  end
-
-  def destroy
-    @favorite = Favorite.find_by(user_id: current_user.id, wine_id: @wine.id)
-    @favorite.destroy
+    favorite = Favorite.create(user_id: current_user.id, wine_id: @wine.id)
+    redirect_to wine_path(@wine)
   end
 
   private
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,4 +2,5 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :wine
 
+  validates :user_id, uniqueness: { scope: :wine_id }
 end
