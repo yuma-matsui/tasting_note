ed53caed29d6424153527989711209121c97b801
yuma-matsui <oshino.iiyo@gmail.com>
2020-10-19 17:01:02 +0900

    微調整

--- app/javascript/look_preview.js
+++ app/javascript/look_preview.js
@@ -1,4 +1,4 @@
-if (document.URL.match( /new_look/ ) ) {
+if (document.URL.match( /new_look/ ) || location.pathname.match( "create_look" ) ) {
   function preview () {
     const ImageList = document.getElementById('image-list');
     const ImageForm = document.getElementById('look_image');
