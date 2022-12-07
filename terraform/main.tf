a8b1653b2915cbbed783982b1d7b69238901cda2
yuma-matsui <oshino.iiyo@gmail.com>
2022-12-07 09:19:50 +0900

    Terraformの設定を行った

---
+++ terraform/main.tf
@@ -0,0 +1,34 @@
+# --------------------------
+# Terraform Configuration
+# --------------------------
+terraform {
+  required_version = ">= 1.3.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# --------------------------
+# Provider
+# --------------------------
+provider "aws" {
+  profile = "terraform"
+  region  = "ap-northeast-1"
+}
+
+provider "aws" {
+  alias   = "virginia"
+  profile = "terraform"
+  region  = "us-east-1"
+}
+
+# --------------------------
+# variables
+# --------------------------
+variable "project" {
+  type    = string
+  default = "tasting-note"
+}
