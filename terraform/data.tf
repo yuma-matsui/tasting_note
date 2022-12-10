a82ce42dc27f15053898ea831483020b67382dad
yuma-matsui <oshino.iiyo@gmail.com>
2022-12-10 23:03:22 +0900

    本番環境用のECSを作成した

--- terraform/data.tf
+++ terraform/data.tf
@@ -5,10 +5,15 @@ data "aws_route53_zone" "tasting_note" {
   name         = "${var.project}.com"
   private_zone = false
 }
-
 # --------------------------
 # IAM User for CI/CD
 # --------------------------
 data "aws_iam_user" "circleci" {
   user_name = "circleci-user"
+}
+# --------------------------
+# Parameter Store for ECS
+# --------------------------
+data "aws_ssm_parameter" "rails_master_key" {
+  name = "tasting-note-masterkey"
 }
---
+++ terraform/ecs.tf
@@ -0,0 +1,58 @@
+# --------------------------
+# Cluster
+# --------------------------
+resource "aws_ecs_cluster" "tasting_note" {
+  name = "${var.project}-cluster"
+}
+# --------------------------
+# task difinition
+# --------------------------
+resource "aws_ecs_task_definition" "rails_task" {
+  family                   = "${var.project}-task"
+  cpu                      = 256
+  memory                   = 512
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+
+  execution_role_arn = aws_iam_role.ecs_task_execution.arn
+  task_role_arn      = aws_iam_role.ecs_task_execution.arn
+
+  container_definitions = data.template_file.container_definitions.rendered
+}
+data "template_file" "container_definitions" {
+  template = file("./task_definitions/rails_container_definitions.json")
+
+  vars = {
+    project               = var.project
+    image                 = aws_ecr_repository.tasting_note.repository_url
+    aws_ssm_parameter_arn = data.aws_ssm_parameter.rails_master_key.arn
+  }
+}
+# --------------------------
+# service
+# --------------------------
+resource "aws_ecs_service" "tasting-note" {
+  name                              = "${var.project}-service"
+  cluster                           = aws_ecs_cluster.tasting_note.arn
+  task_definition                   = aws_ecs_task_definition.rails_task.arn
+  desired_count                     = 2
+  launch_type                       = "FARGATE"
+  platform_version                  = "1.4.0"
+  health_check_grace_period_seconds = 60
+
+  network_configuration {
+    assign_public_ip = true
+    security_groups  = [aws_security_group.ecs_sg.id]
+
+    subnets = [
+      aws_subnet.public_1a.id,
+      aws_subnet.public_1c.id
+    ]
+  }
+
+  load_balancer {
+    target_group_arn = aws_alb_target_group.ecs_target_group.arn
+    container_name   = "${var.project}-container"
+    container_port   = 3000
+  }
+}
