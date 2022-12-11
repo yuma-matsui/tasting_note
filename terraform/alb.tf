e9ab0382a1e353de0433462d3432a8fc9309ddbf
yuma-matsui <oshino.iiyo@gmail.com>
2022-12-11 16:03:04 +0900

    ALBのssl policyを最新版に変更した

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -17,7 +17,7 @@ resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.alb.arn
   port              = 443
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
   certificate_arn   = aws_acm_certificate.tokyo.arn
   default_action {
     type             = "forward"
